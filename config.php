e9348db2c8801f7da9ff784e78a4ccf000d99fbc
Hans-Peter Jungbauer <peter@minasoptics.com.br>
2026-02-04 19:21:33 -0300

    Changed db to prado, SQLEXPRESS22

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+// Constants for config.php
+$debugmode = '0'; // 0 = off, 1 = on
+$sqlexpress = '1'; // 0 = MSSQL, 1 = SQLEXPRESS22
+$maxnumber_of_checkpoints = '6';
+$min_ipl = '0';
+$max_ipl = '10';
+$min_verniz = '4';
+$max_verniz = '20';
+$minintegrationtime = '3';
+$maxintegrationtime = '200';
+$maxnumber_of_average = '100';
+$minlambda = '350';
+$maxlambda = '1000';
+$maxpeaks = '5';
+$min_fft = '0';
+$max_fft = '100';
+$minnoise = '5';
+$maxnoise = '80';
+$max_verniz_ri = 4;
+$max_ipl_ri = 4;
+
+$user = 'procoat';   // Name for database user procoat
+$pw = 'tla';        // Password for databse user i.e tla
+$db = 'prado'; // Database name i.e procoat. Change this to the name of your database
+
+// NEU (Zeile 2):
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}  // ✅
+
+?>
